3f1364415e615db4e66c2c17f939a8cc37639855
marfra99x <marfra99x@gmail.com>
2022-12-04 11:51:08 +0100

    day 4

---
+++ 2022/04/task-1.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <string> 
+
+using namespace std;
+
+int main(){
+    ifstream in("input.txt");
+    
+    int res = 0;
+    while(!in.eof()){
+        int left1,right1,left2,right2;
+        char waste;
+
+        in >> left1 >> waste >> right1 >> waste >> left2 >> waste >> right2;
+
+        if(left1<=left2 && right1>=right2)res++;
+        else if(left2<=left1 && right2>=right1)res++;
+    }
+    cout << res << endl;
+    return 0;
+}
